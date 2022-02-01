f0d9e7f56ae1bd85d85b46c4e993c424ab63b16f
aunshx <aunsh.sb@gmail.com>
2022-02-01 18:23:39 +0530

    about page start

--- client/src/components/layout/About.js
+++ client/src/components/layout/About.js
@@ -4,9 +4,34 @@ import PropTypes from 'prop-types';
 import Navbar from '../navbar/Navbar'
 
 const About = (props) => {
-  return <>
+  return (
+    <>
       <Navbar />
-  </>;
+      <div className='about'>
+        <div className='main-about flex_middle'>
+          <div className='image'>
+            <div className='title'>About</div>
+            <div className='credits'>
+              Photo by{" "}
+              <a
+                href='https://unsplash.com/@nate_dumlao?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
+                style={{ color: "grey" }}
+              >
+                Nathan Dumlao
+              </a>{" "}
+              on{" "}
+              <a
+                href='https://unsplash.com/s/photos/time?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'
+                style={{ color: "grey" }}
+              >
+                Unsplash
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 About.propTypes = {};
