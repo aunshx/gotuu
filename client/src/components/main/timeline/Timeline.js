27c7292bf208806dcbcb03b86fa4072bbebcc8a0
aunshx <aunsh.sb@gmail.com>
2022-02-03 11:06:40 +0530

    removed the poker from datepicker

--- client/src/components/main/timeline/Timeline.js
+++ client/src/components/main/timeline/Timeline.js
@@ -116,27 +116,6 @@ const Timeline = ({
                   height: "350px",
                   filter: "drop-shadow(0px 2px 4px rgba(0,0,0,0.32))",
                   mt: 1.5,
-                  "& .MuiAvatar-root": {
-                    width: 32,
-                    height: 32,
-                    ml: -0.5,
-                    mr: 1,
-                  },
-                  "&:before": {
-                    content: '""',
-                    display: "block",
-                    position: "absolute",
-                    top: 0,
-                    left: 120,
-                    width: 10,
-                    height: 10,
-                    bgcolor: "background.paper",
-                    transform: "translateY(-50%) rotate(45deg)",
-                    zIndex: 0,
-                  },
-                },
-                style: {
-                  transform: "translateX(0px) translateY(-1px)",
                 },
               }}
               transformOrigin={{
