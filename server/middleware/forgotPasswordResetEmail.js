45725d7f71e468b8e5acb094d92237fd8ee8e48d
aunshx <aunsh.sb@gmail.com>
2022-02-17 10:50:21 +0530

    removing default

--- server/middleware/forgotPasswordResetEmail.js
+++ server/middleware/forgotPasswordResetEmail.js
@@ -1,17 +1,29 @@
 const moment = require("moment");
 const config = require("config");
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 
 const sendGridKey = config.get("sendGridAPIKeyEmail");
 
-const sendEmail = async (email, name, organisation, message) => {
-  sgMail.setApiKey(sendGridKey);
-  const msg = {
-    to: "aunsh.tech@gmail.com",
-    from: "ceo@aunsh.com",
-    subject: "Somebody has messaged you!!",
-    text: "New Message",
-    html: `
+const sendEmail = (email, securityCode) => {
+   sgMail.setApiKey(sendGridKey);
+
+  const request = mailjet.post("send", { version: "v3.1" }).request({
+    Messages: [
+      {
+        From: {
+          Email: "gotuu.app@gmail.com",
+          Name: "Gotuu",
+        },
+        To: [
+          {
+            Email: email,
+            Name: "Aunsh",
+          },
+        ],
+        Subject: "Reset Password",
+        TextPart: "Greetings from Mailjet!",
+        HTMLPart: `
         <!DOCTYPE html>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
@@ -38,28 +50,50 @@ const sendEmail = async (email, name, organisation, message) => {
       <td align="center" style="padding:0;">
         <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
           <tr style="height: 10px;">
-            <td align="center" style="padding:5px 0 5px 0;background:#546670;">
-                <img src='https://i.postimg.cc/qB6VG513/a-removebg-preview.png' alt="" width="40" style="height:auto;display:block;" />
-                <h1 style="font-size:20px;margin:5px 0 5px 15px;font-family:Arial,sans-serif;color:#fff;">New Message</h1>
+            <td align="center" style="padding:5px 0 5px 0;background:#2e2e2e;">
+                <img src='https://i.postimg.cc/mDXch8wx/gotuu-Logo-Login.png' alt="" width="60" style="height:auto;display:block;" />
+                <h1 style="font-size:20px;margin:5px 0 5px 0;font-family:Arial,sans-serif;color:#fff;">Reset Password</h1>
             </td>
           </tr>
           <tr>
             <td style="padding:36px 30px 25px 30px;">
               <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
                 <tr>
-                  <td style="padding:0 0 0px 0;color:#153643;" align="center">
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(78, 170, 219);">Name: </h1>
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(59, 58, 58);">${name}</h1>
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(78, 170, 219);">Email: </h1>
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(59, 58, 58);">${email}</h1>
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(78, 170, 219);">Organisation: </h1>
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(59, 58, 58);">${organisation}</h1>
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(78, 170, 219);">Message: </h1>
-                    <h1 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(59, 58, 58);">${message}</h1>
-                    
-                    <p style="margin:40px 0 0px 0;font-size:14px;line-height:24px;font-family:Arial,sans-serif; color: gray;">Date: ${moment().format(
+                  <td style="padding:0 0 20px 0;color:#153643;" align="center">
+                    <h1 style="font-size:18px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: gray;">Your reset code is:</h1>
+                    <h2 style="font-size:14px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: rgb(64, 64, 64);">${securityCode}</h2>
+                    <p style="margin:0 0 20px 0;font-size:14px;line-height:24px;font-family:Arial,sans-serif; color: gray;">Date: ${moment().format(
                       "DD/MM/YYYY"
-                    )} | Time: ${moment().format("hh:mm a")}</p>
+                    )} | Time: ${moment().format(
+          "hh:mm a"
+        )} | Reset Code Validity:10 minutes.</p>
+                  </td>
+                </tr>
+                <tr>
+                    <td style="padding:0 0 20px 0;color:#153643;" align="center">
+                        <div style="border: 1px solid gray;"></div>
+                    </td>
+                </tr>
+                <tr>
+                  <td style="padding:0;">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                      <tr>
+                        <td style="width:260px;padding:0;vertical-align:top;color:#153643;">
+                          <h1 style="font-size:15px;margin:0 0 20px 0;font-family:Arial,sans-serif; color: gray;" align="center">Tips for a new password: </h1>
+                          <p style="margin:0 0 12px 0;font-size:13px;line-height:24px;font-family:Arial,sans-serif;">
+                            <ol style="margin:0 0 12px 0;font-size:13px;line-height:24px;font-family:Arial,sans-serif;">
+                                <li>Password should be of at least 8 characters or more</li>
+                                <li>Password should contain special characters such as '!', '@', '#" and numbers</li>
+                                <li>Avoid disclosing your password to anyone.</li>
+                                <li>Avoid having common places and words as your password.</li>
+                            </ol>
+                          </p>
+                          <p style="margin:0;font-size:13px;line-height:24px;font-family:Arial,sans-serif;" align="center"><a href="https://blog.avast.com/strong-password-ideas"  target="_blank" rel="noreferrer" style="color:#31c22a;text-decoration:underline;">How to create a strong password</a></p>
+                          <p style="margin:0;font-size:10px;line-height:24px;font-family:Arial,sans-serif;" align="center"><a href="https://blog.avast.com/strong-password-ideas"  target="_blank" rel="noreferrer" style="color:#bababa;text-decoration:none;"Unsubscribe</a></p>
+                          <p style="color:#bababa;margin:0;font-size:10px;line-height:10px;font-family:Arial,sans-serif;" align="center">	&copy; Gotuu 2022</p>
+                        </td>
+                      </tr>
+                    </table>
                   </td>
                 </tr>
               </table>
@@ -74,16 +108,16 @@ const sendEmail = async (email, name, organisation, message) => {
   </table>
 </body>
 </html>
-      `,
-  };
-
-  sgMail
-    .send(msg)
-    .then(() => {
+        `,
+      },
+    ],
+  });
+  request
+    .then((result) => {
       console.log("Email Sent");
     })
     .catch((err) => {
-      console.log(err);
+      console.log(err.statusCode);
     });
 };
 
