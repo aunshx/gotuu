2cae6684382ede8e9bae88a7cc46c44744cff0ed
aunshx <aunsh.sb@gmail.com>
2022-03-04 18:02:00 +0530

    register page media query

--- client/src/components/about/walkthrough/HowTo.js
+++ client/src/components/about/walkthrough/HowTo.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Para from './Para';
 
 const HowTo = ({ innerRef, secondInnerRef }) => {
   return (
     <div className='pages' id='walkthrough-about' ref={innerRef}>
       <div className='title flex_middle'>Walkthrough</div>
-      <div className='flex_middle mini-title'>I. Using Go without login</div>
       <div className='page-details-walkthrough' ref={secondInnerRef}>
-        <div className='gif'></div>
-        <div className='writing app'>jj
-          <div>s</div>
+        <div className='writing app'>
+          <Para srNo={1} />
           <div>s</div>
           <div>s</div>
           <div>s</div>
--- client/src/components/about/walkthrough/Para.js
+++ client/src/components/about/walkthrough/Para.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Para = props => {
+const Para = ({ srNo }) => {
   return (
-    <div className='walkthrough'>
-        <div className="title">
-
+    <div className='walkthrough-para'>
+        <div className="title-para flex_middle">
+            <span style={{ marginRight: '0.3em' }}>{srNo}.</span> Whats the need for security questions during registration?
         </div>
-        <div className="image">
-
+        <div className="image-para">
+            <img src="" alt="" />
         </div>
         <div className="caption">
-            
+        <ul>
+            <li>Security questions are used if you forget your password.</li>
+            <li>Always keep the answers to your security questions stored securely.</li>
+        </ul>
         </div>
     </div>
   )
