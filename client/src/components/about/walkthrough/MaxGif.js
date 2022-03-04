1e2e5198f998fd2473e27d3bd6d934375651cccd
aunshx <aunsh.sb@gmail.com>
2022-03-04 21:12:53 +0530

    close positioning

--- client/src/components/about/walkthrough/MaxGif.js
+++ client/src/components/about/walkthrough/MaxGif.js
@@ -3,18 +3,19 @@ import CloseIcon from "@mui/icons-material/Close";
 
 const MaxGif = ({ close, image, altImg, title }) => {
   return (
-    <div className='max-gif-card app'>
-        <div className="triple_grid">
-            <div></div>
-            <div className='title' >
-                {title}
-            </div>
-            <div>
-                <CloseIcon onClick={close} className='cancel cursor_pointer' />
-            </div>
+    <div className='max-gif-card'>
+      <div className='triple_grid'>
+        <div></div>
+        <div className='title flex_middle'>{title}</div>
+        <div className='flex_right icons'>
+          <CloseIcon onClick={close} className='cancel cursor_pointer' style={{ fontSize: 18 }} />
         </div>
+      </div>
+      <div className='flex_middle'>
+        <img src={image} alt={altImg} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default MaxGif
