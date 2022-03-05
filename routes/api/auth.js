87dc978261c203fcf2dc3c5d8dfd30b37c2fea08
aunshx <aunsh.sb@gmail.com>
2022-03-06 03:44:12 +0530

    removing unnecessary packages from the server

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
 const jwt = require("jsonwebtoken");
-const random = require("random");
 const config = require("config");
 const { check, validationResult } = require("express-validator")
 
